fb754cfcdcc4b811436987141c7a9d4bd6b10fbb
tanzahmed <ahmedtufailtanzeem@gmail.com>
2018-08-06 19:53:12 +0530

    Good progress till Emoji

---
+++ src/01_print_function.py
@@ -0,0 +1,37 @@
+# Simple print's with quote
+print("\nSimple print with quotes")
+print("Hello World in double quotes")
+print('Hello World in single quotes')
+print("Hello 'Tanzeem'")
+print('Hello "Tanzeem Ahmed"')
+
+# Escape sequence
+print("\nEscape Sequence")
+print("Hello \"Tanzeem\"")
+print('Hello \'Tanzeem Ahmed\'')
+print('Hello \nTanzeem Ahmed')
+print('Hello Tanzeem\tAhmed')
+print('Hello Tanzeem  \bAhmed #\b')
+
+# Escape characters as Normal Text
+print("Escape characters as normal text")
+print("Hello \\nTanzeem Ahmed")
+print("Hello \\tTanzeem Ahmed")
+print("Hello with 4 backslash \\\\\\\\")
+print("\\\" - \\\'")
+
+# Exercise
+print("\nExercise")
+print("this is \\\\ double backslash")
+print("this is /\\/\\/\\/\\ mountain")
+print("he is \t Awesome!!!")
+print("\\ \t \\\" \t \" \t \' \t \\b \t\\n \t\\t")
+
+# Raw string
+print("\nRaw string")
+print(r"this is /\/\/\/\ mountain")
+
+# Emoji
+print("\nEmoji")
+print("🍺")
+print("\U0001F37E")
